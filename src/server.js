144a95074ac79ca1289c875851773101da0a5367
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-06-07 22:24:25 +0300

    add status to response

--- src/server.js
+++ src/server.js
@@ -26,9 +26,11 @@ export const setupServer = () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
 
-    res
-      .status(200)
-      .json({ message: 'Successfully found contacts!', data: contacts });
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
   });
 
   app.get('/contacts/:contactId', async (req, res, next) => {
@@ -44,6 +46,7 @@ export const setupServer = () => {
       }
 
       res.status(200).json({
+        status: 200,
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
