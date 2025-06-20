7f695f4535a96a791c4e21fc067a6d0151c92c67
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-06-20 20:07:30 +0300

    fixed isValidId function

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,11 @@
 import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
-export const isValidId = (req, res, next) => {
-  const { contactId } = req.params;
-  if (!isValidObjectId(contactId)) {
-    throw createHttpError(400, 'Bad request');
-  }
-  next();
-};
+export const isValidId =
+  (idName = 'id') =>
+  (req, res, next) => {
+    if (!isValidObjectId(req.params[idName])) {
+      throw createHttpError(400, `Invalid MongoDB id: ${idName}`);
+    }
+    next();
+  };
