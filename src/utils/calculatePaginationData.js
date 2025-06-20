4c8bd495822ae48dc88f117cc00154140c864bd0
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-06-20 20:45:58 +0300

    add error when page exceeds totalPages

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,7 +1,16 @@
+import createHttpError from 'http-errors';
+
 export const calculatePaginationData = (count, perPage, page) => {
   const totalPages = Math.ceil(count / perPage);
   const hasNextPage = Boolean(totalPages - page);
   const hasPreviousPage = page !== 1;
+
+  if (totalPages > 0 && page > totalPages) {
+    throw createHttpError(
+      400,
+      `Page ${page} exceeds total pages (${totalPages})`,
+    );
+  }
 
   return {
     page,
