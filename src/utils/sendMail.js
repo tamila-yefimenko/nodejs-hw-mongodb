9aa723a2a10b0bfe750262b1b706e41f7cf0c614
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-07-05 14:14:04 +0300

    changed sendMail

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -6,6 +6,7 @@ import createHttpError from 'http-errors';
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
   port: Number(getEnvVar(SMTP.SMTP_PORT)),
+  secure: true,
   auth: {
     user: getEnvVar(SMTP.SMTP_USER),
     pass: getEnvVar(SMTP.SMTP_PASSWORD),
