0b4f7432ee2cf165ee3aa3c8c422a405fcc69c2c
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-07-11 21:57:33 +0300

    homework 7

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -28,6 +29,8 @@ export const setupServer = () => {
   );
 
   app.use(router);
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
 
